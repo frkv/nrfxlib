nrfconnect/sdk-nrfxlib

Initial release of nrf_oberon and nrf_security documentation
- Added API documentation for nrf_oberon
- Added API documentation for nrf_security
- Added License headers in c-files/h-files
- RSA: Renamed export to export_fn as it is a reserved keyword in C++

Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

nrf_security/include/mbedcrypto_glue/mbedtls/aes_alt.h
@@ -1,56 +1,65 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+/**@file
+ * @addtogroup mbedtls_glue_aes
+ * @{
+ */
 #ifndef MBEDTLS_AES_ALT_H
 #define MBEDTLS_AES_ALT_H
 
 #if !defined(MBEDTLS_CONFIG_FILE)
-#include "config.h"
+#include "../config.h"
 #else
 #include MBEDTLS_CONFIG_FILE
 #endif
 
-
-#define MBEDTLS_CC3XX_AES_CONTEXT_WORDS         (24)
-#define MBEDTLS_DEFAULT_AES_CONTEXT_WORDS       (70)
-#define MBEDTLS_DEFAULT_AES_XTS_CONTEXT_WORDS   (140)
+#define MBEDTLS_CC3XX_AES_CONTEXT_WORDS         (24)    //!< AES context size in words in nrf_cc310_mbedcrypto library.
+#define MBEDTLS_DEFAULT_AES_CONTEXT_WORDS       (70)    //!< AES context size in words in vanilla mbed TLS.
+#define MBEDTLS_DEFAULT_AES_XTS_CONTEXT_WORDS   (140)   //!< AES XTS context size in words in vanilla mbed TLS.
 
 #if defined(MBEDTLS_AES_ALT)
 
 #include <stdint.h>
 
-/**
- * \brief The AES context-type definition.
+
+/** @brief Typedef of mbedcrypto AES glue context
  */
 typedef struct mbedtls_aes_context
 {
-    void* handle;
+    void* handle;   //!< Pointer to the function table in an initialized glue context.
     union
     {
 #if defined(CONFIG_CC310_MBEDTLS_AES_C)
-        uint32_t buffer_cc3xx[MBEDTLS_CC3XX_AES_CONTEXT_WORDS];
-#endif
+        uint32_t buffer_cc3xx[MBEDTLS_CC3XX_AES_CONTEXT_WORDS];         //!< Array the size of an AES context in the nrf_cc310_mbedcrypto library.
+#endif /* CONFIG_CC310_MBEDTLS_AES_C */
 #if defined(CONFIG_VANILLA_MBEDTLS_AES_C)
-        uint32_t buffer_default[MBEDTLS_DEFAULT_AES_CONTEXT_WORDS];
-#endif
-        uint32_t dummy;
-    } buffer;
+        uint32_t buffer_default[MBEDTLS_DEFAULT_AES_CONTEXT_WORDS];     //!< Array the size of an AES context in vanilla mbed TLS.
+#endif /* CONFIG_VANILLA_MBEDTLS_AES_C */
+        uint32_t dummy;                                                 //!< Dummy value in case no backend is enabled.
+    } buffer;                                                           //!< Union with size of the largest enabled backend context.
 } mbedtls_aes_context;
 
-#if defined(MBEDTLS_CIPHER_MODE_XTS)
-/**
- * \brief The AES XTS context-type definition.
+
+/** @brief Typedef of mbedcrypto glue AES XTS context
  */
 typedef struct mbedtls_aes_xts_context
 {
-    void* handle;
+    void* handle;   //!< Pointer to the function table in an initialized glue context.
     union
     {
 #if defined(CONFIG_VANILLA_MBEDTLS_CIPHER_MODE_XTS)
-        uint32_t buffer_default[MBEDTLS_DEFAULT_AES_XTS_CONTEXT_WORDS];
-#endif
-        uint32_t dummy;
-    } buffer;
+        uint32_t buffer_default[MBEDTLS_DEFAULT_AES_XTS_CONTEXT_WORDS];     //!< Array the size of the memory required for an AES XTS context in vanilla mbed TLS.
+#endif /* CONFIG_VANILLA_MBEDTLS_CIPHER_MODE_XTS */
+        uint32_t dummy;                                                     //!< Dummy value in case no backend is enabled.
+    } buffer;                                                               //!< Array with length equal to the size of the largest enabled backend.
 } mbedtls_aes_xts_context;
-#endif /* MBEDTLS_CIPHER_MODE_XTS */
 
 #endif /* MBEDTLS_AES_ALT */
 
 #endif /* MBEDTLS_AES_ALT_H */
+
+/** @} */

nrf_security/include/mbedcrypto_glue/mbedtls/ccm_alt.h
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+/**@file
+ * @addtogroup mbedcrypto_glue_aes_ccm
+ * @{
+ */
 #ifndef MBEDTLS_CCM_ALT_H
 #define MBEDTLS_CCM_ALT_H
 
@@ -7,9 +17,14 @@
 #include MBEDTLS_CONFIG_FILE
 #endif
 
-#include "mbedtls/cipher.h"
-
+/**
+ * @brief Context size of AES CCM in words in the mbed_cc310_mbedcrypto library.
+ */
 #define MBEDTLS_CC3XX_CCM_CONTEXT_WORDS     (24)
+
+/**
+ * @brief Context size of AES CCM in words in vanilla mbed TLS.
+ */
 #define MBEDTLS_DEFAULT_CCM_CONTEXT_WORDS   ((sizeof(mbedtls_cipher_context_t) + 3) / 4)
 
 #if defined(MBEDTLS_CCM_ALT)
@@ -17,25 +32,27 @@
 #include <stdint.h>
 
 /**
- * \brief The CCM context-type definition.
+ * @brief Typedef of mbedcrypto AES CCM glue context
  */
 typedef struct mbedtls_ccm_context
 {
 #if defined(CONFIG_GLUE_MBEDTLS_CCM_C)
-    void* handle;
+    void* handle;                                                       //!< Pointer to the function table in an initialized glue context.
 #endif
     union
     {
 #if defined(CONFIG_CC310_MBEDTLS_CCM_C)
-        uint32_t buffer_cc3xx[MBEDTLS_CC3XX_CCM_CONTEXT_WORDS];
-#endif
+        uint32_t buffer_cc3xx[MBEDTLS_CC3XX_CCM_CONTEXT_WORDS];         //!< Array the size of an AES CCM context in the nrf_cc310_mbedcrypto library.
+#endif /* CONFIG_CC310_MBEDTLS_CCM_C */
 #if defined(CONFIG_VANILLA_MBEDTLS_CCM_C)
-        uint32_t buffer_default[MBEDTLS_DEFAULT_CCM_CONTEXT_WORDS];
-#endif
-        uint32_t dummy;
-    } buffer;
+        uint32_t buffer_default[MBEDTLS_DEFAULT_CCM_CONTEXT_WORDS];     //!< Array the size of an AES context in vanilla mbed TLS.
+#endif /* CONFIG_VANILLA_MBEDTLS_CCM_C */
+        uint32_t dummy;                                                 //!< Dummy value in case no backend is enabled.
+    } buffer;                                                           //!< Union with size of the largest enabled backend context.
 } mbedtls_ccm_context;
 
 #endif /* MBEDTLS_CCM_ALT */
 
 #endif /* MBEDTLS_CCM_ALT_H */
+
+/** @} */

nrf_security/include/mbedcrypto_glue/mbedtls/platform_alt.h
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+/**@file
+ * @defgroup mbedcrypto_glue
+ * @{
+ * @brief The mbedcrypto glue is a crypto toolbox glue layer coupling
+ *        software and hardware-accelerated cryptography. The glue layer is implemented 
+ *        using the mbed TLS ALT layer (also knonwn as mbed TLS backend) and 
+ *        is usable through regluar mbed TLS front ends.
+ *
+ * @note    If the functionality doesn't benefit from hardware-accelerated implementation
+ *          or isn't supported, the default behavior is to use vanilla mbed TLS.
+ * @}
+ * @defgroup mbedcrypto_glue_platform_alt mbedcrypto platform glue
+ * @ingroup mbedcrypto_glue
+ * @{
+ * @brief This is a glue layer for mbedcrypto platform abstraction
+ */
 #ifndef PLATFORM_ALT_H
 #define PLATFORM_ALT_H
 
@@ -9,16 +31,23 @@
 #include MBEDTLS_CONFIG_FILE
 #endif
 
-#define MBEDTLS_CC3XX_PLATFORM_CONTEXT_WORDS    (6)
+#define MBEDTLS_CC3XX_PLATFORM_CONTEXT_WORDS    (6)     //!<  Context size of of type used to initialize mbedtls in words in the mbed_cc310_mbedcrypto library.
 
+/**@brief Typedef of mbedcrypto platform glue context.
+ *
+ * @details This context is used when initializing/deinitializing mbed TLS using
+ *          @c mbedtls_platform_setup and @c mbedtls_platform_teardown, which is
+ *          required when using hardware-accelerated cryptograpy.
+ */
 typedef struct
 {
 #if defined(MBEDTLS_CC3XX_BACKEND)
-    uint32_t cc3xx[MBEDTLS_CC3XX_PLATFORM_CONTEXT_WORDS];
-#endif
-#if !defined(MBEDTLS_CC3XX_BACKEND)
-    uint8_t dummy;
+    uint32_t cc3xx[MBEDTLS_CC3XX_PLATFORM_CONTEXT_WORDS];   //!< Array the size of mbed TLS init context in the nrf_cc310_mbedcrypto library.
+#else /* MBEDTLS_CC3XX_BACKEND */
+    uint8_t dummy;                                          //!< Dummy value in case nrc_cc310_mbedcrypto backend is not enabled.
 #endif
 } mbedtls_platform_context;
 
 #endif
+
+/** @} */

nrf_security/include/mbedcrypto_glue/mbedtls/rsa_alt.h
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+/**@file
+ * @addtogroup mbedcrypto_glue_rsa
+ * @{
+ */
 #ifndef MBEDTLS_RSA_ALT_H
 #define MBEDTLS_RSA_ALT_H
 
@@ -7,36 +17,36 @@
 #include MBEDTLS_CONFIG_FILE
 #endif
 
-
-#define MBEDTLS_CC3XX_RSA_CONTEXT_WORDS 52
-#define MBEDTLS_DEFAULT_RSA_CONTEXT_WORDS 43
+#define MBEDTLS_CC3XX_RSA_CONTEXT_WORDS         (52)    //!< Context size in words in nrf_cc310_mbedcrypto library in words.
+#define MBEDTLS_DEFAULT_RSA_CONTEXT_WORDS       (43)    //!< Context size in words in vanilla mbed TLS in words.
 
 #if defined(MBEDTLS_RSA_ALT)
 
 #include <stdint.h>
 
-/**
- * \brief The RSA context-type definition.
+/**@brief Typedef for the mbedtls RSA glue context.
  */
 typedef struct mbedtls_rsa_context
 {
 #if defined(CONFIG_GLUE_MBEDTLS_RSA_C)
-    void* handle;
-    int padding;
-    int hash_id;
-#endif
+    void* handle;                                   //!< Pointer to the function table in an initialized glue context.
+    int padding;                                    //!< The padding mode for the RSA operations.
+    int hash_id;                                    //!< The hash digest algorithm used for RSA operations.
+#endif /* CONFIG_GLUE_MBEDTLS_RSA_C */
     union
     {
 #if defined(CONFIG_CC310_MBEDTLS_RSA_C)
-        uint32_t buffer_cc3xx[MBEDTLS_CC3XX_RSA_CONTEXT_WORDS];
-#endif
+        uint32_t buffer_cc3xx[MBEDTLS_CC3XX_RSA_CONTEXT_WORDS];     //!< Array the size of an RSA context in the nrf_cc310_mbedcrypto library.
+#endif /* CONFIG_CC310_MBEDTLS_RSA_C */
 #if defined(CONFIG_VANILLA_MBEDTLS_RSA_C)
-        uint32_t buffer_default[MBEDTLS_DEFAULT_RSA_CONTEXT_WORDS];
-#endif
-        uint32_t dummy;
-    } buffer;
+        uint32_t buffer_default[MBEDTLS_DEFAULT_RSA_CONTEXT_WORDS]; //!< Array the size of an RSA context in vanilla mbed TLS.
+#endif /* CONFIG_VANILLA_MBEDTLS_RSA_C */
+        uint32_t dummy;                             //!< Dummy value in case no backend is enabled.
+    } buffer;                                       //!< Union with size of the largest enabled backend context.
 } mbedtls_rsa_context;
 
 #endif /* MBEDTLS_RSA_ALT */
 
 #endif /* MBEDTLS_RSA_ALT_H */
+
+/** @} */

frkv/nrfxlib

nrf_security/include/mbedcrypto_glue/mbedtls/aes_alt.h
@@ -1,56 +1,65 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+/**@file
+ * @addtogroup mbedtls_glue_aes
+ * @{
+ */
 #ifndef MBEDTLS_AES_ALT_H
 #define MBEDTLS_AES_ALT_H
 
 #if !defined(MBEDTLS_CONFIG_FILE)
-#include "config.h"
+#include "../config.h"
 #else
 #include MBEDTLS_CONFIG_FILE
 #endif
 
-
-#define CC310_MBEDTLS_AES_CONTEXT_WORDS         (24)
-#define VANILLA_MBEDTLS_AES_CONTEXT_WORDS       (70)
-#define VANILLA_MBEDTLS_AES_XTS_CONTEXT_WORDS   (140)
+#define CC310_MBEDTLS_AES_CONTEXT_WORDS         (24)    //!< AES context size in words in nrf_cc310_mbedcrypto library.
+#define VANILLA_MBEDTLS_AES_CONTEXT_WORDS       (70)    //!< AES context size in words in vanilla mbed TLS.
+#define VANILLA_MBEDTLS_AES_XTS_CONTEXT_WORDS   (140)   //!< AES XTS context size in words in vanilla mbed TLS.
 
 #if defined(MBEDTLS_AES_ALT)
 
 #include <stdint.h>
 
-/**
- * \brief The AES context-type definition.
+
+/** @brief Typedef of mbedcrypto AES glue context
  */
 typedef struct mbedtls_aes_context
 {
-    void* handle;
+    void* handle;   //!< Pointer to the function table in an initialized glue context.
     union
     {
 #if defined(CONFIG_CC310_MBEDTLS_AES_C)
-        uint32_t buffer_cc310[CC310_MBEDTLS_AES_CONTEXT_WORDS];
-#endif
+        uint32_t buffer_cc310[CC310_MBEDTLS_AES_CONTEXT_WORDS];                //!< Array the size of an AES context in the nrf_cc310_mbedcrypto library.
+#endif /* CONFIG_CC310_MBEDTLS_AES_C */
 #if defined(CONFIG_VANILLA_MBEDTLS_AES_C)
-        uint32_t buffer_vanilla_mbedtls[VANILLA_MBEDTLS_AES_CONTEXT_WORDS];
-#endif
-        uint32_t dummy;
-    } buffer;
+        uint32_t buffer_vanilla_mbedtls[VANILLA_MBEDTLS_AES_CONTEXT_WORDS];    //!< Array the size of an AES context in vanilla mbed TLS.
+#endif /* CONFIG_VANILLA_MBEDTLS_AES_C */
+        uint32_t dummy;                                                        //!< Dummy value in case no backend is enabled.
+    } buffer;                                                                  //!< Union with size of the largest enabled backend context.
 } mbedtls_aes_context;
 
-#if defined(MBEDTLS_CIPHER_MODE_XTS)
-/**
- * \brief The AES XTS context-type definition.
+
+/** @brief Typedef of mbedcrypto glue AES XTS context
  */
 typedef struct mbedtls_aes_xts_context
 {
-    void* handle;
+    void* handle;   //!< Pointer to the function table in an initialized glue context.
     union
     {
 #if defined(CONFIG_VANILLA_MBEDTLS_CIPHER_MODE_XTS)
-        uint32_t buffer_vanilla_mbedtls[VANILLA_MBEDTLS_AES_XTS_CONTEXT_WORDS];
-#endif
-        uint32_t dummy;
-    } buffer;
+        uint32_t buffer_vanilla_mbedtls[VANILLA_MBEDTLS_AES_XTS_CONTEXT_WORDS];    //!< Array the size of the memory required for an AES XTS context in vanilla mbed TLS.
+#endif /* CONFIG_VANILLA_MBEDTLS_CIPHER_MODE_XTS */
+        uint32_t dummy;                                                            //!< Dummy value in case no backend is enabled.
+    } buffer;                                                                      //!< Array with length equal to the size of the largest enabled backend.
 } mbedtls_aes_xts_context;
-#endif /* MBEDTLS_CIPHER_MODE_XTS */
 
 #endif /* MBEDTLS_AES_ALT */
 
 #endif /* MBEDTLS_AES_ALT_H */
+
+/** @} */

nrf_security/include/mbedcrypto_glue/mbedtls/ccm_alt.h
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+/**@file
+ * @addtogroup mbedcrypto_glue_aes_ccm
+ * @{
+ */
 #ifndef MBEDTLS_CCM_ALT_H
 #define MBEDTLS_CCM_ALT_H
 
@@ -9,33 +19,42 @@
 
 #include "mbedtls/cipher.h"
 
+/**
+ * @brief Context size of AES CCM in words in the mbed_cc310_mbedcrypto library.
 #define CC310_MBEDTLS_CCM_CONTEXT_WORDS     (24)
 #define VANILLA_MBEDTLS_CCM_CONTEXT_WORDS   ((sizeof(mbedtls_cipher_context_t) + 3) / 4)
+
+/**
+ * @brief Context size of AES CCM in words in vanilla mbed TLS.
+ */
 
 #if defined(MBEDTLS_CCM_ALT)
 
 #include <stdint.h>
 
 /**
- * \brief The CCM context-type definition.
+ * @brief Typedef of mbedcrypto AES CCM glue context
  */
 typedef struct mbedtls_ccm_context
 {
 #if defined(CONFIG_GLUE_MBEDTLS_CCM_C)
-    void* handle;
+    void* handle;                                                             //!< Pointer to the function table in an initialized glue context.
 #endif
     union
     {
 #if defined(CONFIG_CC310_MBEDTLS_CCM_C)
-        uint32_t buffer_cc310[CC310_MBEDTLS_CCM_CONTEXT_WORDS];
-#endif
+        uint32_t buffer_cc310[CC310_MBEDTLS_CCM_CONTEXT_WORDS];               //!< Array the size of an AES CCM context in the nrf_cc310_mbedcrypto library.
+
+#endif /* CONFIG_CC310_MBEDTLS_CCM_C */
 #if defined(CONFIG_VANILLA_MBEDTLS_CCM_C)
-        uint32_t buffer_vanilla_mbedtls[VANILLA_MBEDTLS_CCM_CONTEXT_WORDS];
-#endif
-        uint32_t dummy;
-    } buffer;
+        uint32_t buffer_vanilla_mbedtls[VANILLA_MBEDTLS_CCM_CONTEXT_WORDS];   //!< Array the size of an AES CCM context in vanilla mbed TLS.
+#endif /* CONFIG_VANILLA_MBEDTLS_CCM_C */
+        uint32_t dummy;                                                       //!< Dummy value in case no backend is enabled.
+    } buffer;                                                                 //!< Union with size of the largest enabled backend context.
 } mbedtls_ccm_context;
 
 #endif /* MBEDTLS_CCM_ALT */
 
 #endif /* MBEDTLS_CCM_ALT_H */
+
+/** @} */

nrf_security/include/mbedcrypto_glue/mbedtls/platform_alt.h
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+/**@file
+ * @defgroup mbedcrypto_glue
+ * @{
+ * @brief The mbedcrypto glue is a crypto toolbox glue layer coupling
+ *        software and hardware-accelerated cryptography. The glue layer is implemented 
+ *        using the mbed TLS ALT layer (also knonwn as mbed TLS backend) and 
+ *        is usable through regluar mbed TLS front ends.
+ *
+ * @note    If the functionality doesn't benefit from hardware-accelerated implementation
+ *          or isn't supported, the default behavior is to use vanilla mbed TLS.
+ * @}
+ * @defgroup mbedcrypto_glue_platform_alt mbedcrypto platform glue
+ * @ingroup mbedcrypto_glue
+ * @{
+ * @brief This is a glue layer for mbedcrypto platform abstraction
+ */
 #ifndef PLATFORM_ALT_H
 #define PLATFORM_ALT_H
 
@@ -9,16 +31,23 @@
 #include MBEDTLS_CONFIG_FILE
 #endif
 
-#define CC310_MBEDTLS_PLATFORM_CONTEXT_WORDS    (6)
+#define CC310_MBEDTLS_PLATFORM_CONTEXT_WORDS    (6)//!<  Context size of of type used to initialize mbedtls in words in the mbed_cc310_mbedcrypto library.
 
+/**@brief Typedef of mbedcrypto platform glue context.
+ *
+ * @details This context is used when initializing/deinitializing mbed TLS using
+ *          @c mbedtls_platform_setup and @c mbedtls_platform_teardown, which is
+ *          required when using hardware-accelerated cryptograpy.
+ */
 typedef struct
 {
 #if defined(CONFIG_CC310_BACKEND)
-    uint32_t cc310[CC310_MBEDTLS_PLATFORM_CONTEXT_WORDS];
-#endif
+    uint32_t cc310[CC310_MBEDTLS_PLATFORM_CONTEXT_WORDS];  //!< Array the size of mbed TLS init context in the nrf_cc310_mbedcrypto library.
+#else /* MBEDTLS_CC3XX_BACKEND */
 #if !defined(CONFIG_CC310_BACKEND)
-    uint8_t dummy;
 #endif
 } mbedtls_platform_context;
 
 #endif
+
+/** @} */

nrf_security/include/mbedcrypto_glue/mbedtls/rsa_alt.h
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+/**@file
+ * @addtogroup mbedcrypto_glue_rsa
+ * @{
+ */
 #ifndef MBEDTLS_RSA_ALT_H
 #define MBEDTLS_RSA_ALT_H
 
@@ -7,36 +17,36 @@
 #include MBEDTLS_CONFIG_FILE
 #endif
 
-
-#define CC310_MBEDTLS_RSA_CONTEXT_WORDS 52
-#define VANILLA_MBEDTLS_RSA_CONTEXT_WORDS 43
+#define CC310_MBEDTLS_RSA_CONTEXT_WORDS       (52)    //!< Context size in words in nrf_cc310_mbedcrypto library in words.
+#define VANILLA_MBEDTLS_RSA_CONTEXT_WORDS     (43)    //!< Context size in words in vanilla mbed TLS in words.
 
 #if defined(MBEDTLS_RSA_ALT)
 
 #include <stdint.h>
 
-/**
- * \brief The RSA context-type definition.
+/**@brief Typedef for the mbedtls RSA glue context.
  */
 typedef struct mbedtls_rsa_context
 {
 #if defined(CONFIG_GLUE_MBEDTLS_RSA_C)
-    void* handle;
-    int padding;
-    int hash_id;
-#endif
+    void* handle;                                   //!< Pointer to the function table in an initialized glue context.
+    int padding;                                    //!< The padding mode for the RSA operations.
+    int hash_id;                                    //!< The hash digest algorithm used for RSA operations.
+#endif /* CONFIG_GLUE_MBEDTLS_RSA_C */
     union
     {
 #if defined(CONFIG_CC310_MBEDTLS_RSA_C)
-        uint32_t buffer_cc310[CC310_MBEDTLS_RSA_CONTEXT_WORDS];
-#endif
+        uint32_t buffer_cc310[CC310_MBEDTLS_RSA_CONTEXT_WORDS];               //!< Array the size of an RSA context in the nrf_cc310_mbedcrypto library.
+#endif /* CONFIG_CC310_MBEDTLS_RSA_C */
 #if defined(CONFIG_VANILLA_MBEDTLS_RSA_C)
-        uint32_t buffer_vanilla_mbedtls[VANILLA_MBEDTLS_RSA_CONTEXT_WORDS];
-#endif
-        uint32_t dummy;
-    } buffer;
+        uint32_t buffer_vanilla_mbedtls[VANILLA_MBEDTLS_RSA_CONTEXT_WORDS];   //!< Array the size of an RSA context in vanilla mbed TLS.
+#endif /* CONFIG_VANILLA_MBEDTLS_RSA_C */
+        uint32_t dummy;                             //!< Dummy value in case no backend is enabled.
+    } buffer;                                       //!< Union with size of the largest enabled backend context.
 } mbedtls_rsa_context;
 
 #endif /* MBEDTLS_RSA_ALT */
 
 #endif /* MBEDTLS_RSA_ALT_H */
+
+/** @} */